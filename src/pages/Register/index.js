1e78e6f6031ff522021bb5520985f112d87aaa4b
Vettore0 <54145455+Vettore0@users.noreply.github.com>
2019-10-30 11:50:40 -0300

    Add telas

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -3,18 +3,18 @@ import {
     View, Image, TextInput, ImageBackground, StyleSheet
 } from 'react-native'
 import background from '../../assets/background.jpg'
-
+import logo from '../../assets/logo.png'
 function Register() {
     return (
         <ImageBackground source={background} style={styles.background} >
 
-            <View style={styles.logo}>
-                <Image source={require('../../assets/logo.png')} />
-            </View>
+
+            <Image source={logo}/>
+
             <View style={styles.viewRegister}>
-                <TextInput style = {styles.viewRegister} />
-                <TextInput style = {styles.viewRegister} />
-                <TextInput style = {styles.viewRegister}/>
+                <TextInput style={styles.textInput} />
+                <TextInput style={styles.textInput} />
+                <TextInput style={styles.textInput} />
             </View>
 
 
@@ -25,29 +25,26 @@ function Register() {
 
 const styles = StyleSheet.create({
     background: {
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center'
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
 
     },
     logo: {
         marginTop: 8,
-        position: "absolute",
         width: 350,
         height: 350,
         alignItems: "center",
     },
     viewRegister: {
-        marginTop: 350,
+        marginTop: 5,
         alignItems: "center",
-       
-        width:"90%",
-        color:'#ffffff'
-        
+        color: '#ffffff'
+
 
     },
-    textInput:{
-        borderBottomColor:'#ffffff',
+    textInput: {
+        borderBottomColor: '#2f3236',
         borderBottomWidth: 1,
     }
 })
