2bac67265592292006e77de9c5f5251320f3358f
Vettore0 <54145455+Vettore0@users.noreply.github.com>
2019-10-31 11:49:52 -0300

    Tela de cadastro e icones

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,39 +1,71 @@
 import React from 'react'
-
-import {
-    View,
-    Text,
-    Image,
-    StyleSheet,
-} from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import background from '../../assets/background.jpg' //Imagem de fundo
+import logo from '../../assets/logo.png'
 
 function Login() {
     return (
-        <View style = {styles.container}>
-            <Image source = {require ('../../assets/background.jpg')} />
+        <ImageBackground source={background} style={styles.background}>
+            <Image source={logo} />
 
-            <View style = {styles.logo}>
-                <Image source = {require('../../assets/logo.png')} />
+            <View style={styles.viewLogin}>
+                <View style={styles.sectionStyle}>
+                    <Icon name="email" color="#ffffff" style={styles.iconsStyle} ></Icon>
+                    <TextInput placeholder='Digite seu e-mail' placeholderTe0xtColor='#fff' style={styles.input} />
+                </View>
+                <View>
+                    <Icon name="lock" color="#ffffff" ></Icon>
+                    <TextInput placeholder='Digite sua senha' placeholderTextColor='#fff' style={styles.input} />
+                </View>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text>Login</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text>Cadastrar</Text>
+                </TouchableOpacity>
             </View>
-
-        </View>
-
-
+        </ImageBackground>
     )
 }
+
+//Nossos estilos de páginas
 const styles = StyleSheet.create({
-container:{
-    alignItems:"center",
-    flex: 1     
-},
-logo:{
-    position: "absolute",
-    width: 200,
-    height: 200,
-    alignItems: "center",
-},
-
-
-
+    background: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    viewLogin: {
+        backgroundColor: '#2F3236',
+        width: '90%',
+        padding: 15,
+        marginTop: 40
+    },
+    input: {
+        borderBottomColor: '#fff',
+        borderBottomWidth: 1,
+    },
+    button: {
+        backgroundColor: '#fff',
+        marginTop: 15,
+        padding: 5,
+        alignItems: 'center'
+    },
+    sectionStyle: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    iconsStyle: {
+        position: "absolute",
+        paddingRight: 280,
+        resizeMode: 'stretch',
+        fontSize: 30
+    }
 })
+
 export default Login
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,52 +1,105 @@
 import React from 'react'
 import {
-    View, Image, TextInput, ImageBackground, StyleSheet
+    View,
+    Image,
+    TextInput,
+    ImageBackground,
+    StyleSheet,
+    Picker,
+    TouchableOpacity,
+    Text,
+    ScrollView,
 } from 'react-native'
 import background from '../../assets/background.jpg'
 import logo from '../../assets/logo.png'
 function Register() {
     return (
+        <ScrollView>
         <ImageBackground source={background} style={styles.background} >
 
 
-            <Image source={logo}/>
+            <Image source={logo} />
 
             <View style={styles.viewRegister}>
-                <TextInput style={styles.textInput} />
-                <TextInput style={styles.textInput} />
-                <TextInput style={styles.textInput} />
+
+                <TextInput style={styles.textInput} placeholder="Digite seu nome" />
+                <TextInput style={styles.textInput} placeholder="Digite seu e-mail" />
+                <TextInput style={styles.textInput} placeholder="DIgite sua senha" />
+
+                <View style={styles.picker}>
+
+                    <Picker >
+                        <Picker.Item label="Desenvolvedor" value="Desenvolvedor" />
+                        <Picker.Item label="Estudante" value="Estudante" />
+                        <Picker.Item label="Professor" value="Professor" />
+                        <Picker.Item label="Fisico" value="Fisico" />
+                    </Picker>
+
+                </View>
+                <TouchableOpacity style={styles.btn}>
+                    <Text style={styles.textBtn}>
+                        Cadastrar
+                    </Text>
+                </TouchableOpacity>
+
             </View>
 
 
+
         </ImageBackground>
+        </ScrollView>
 
     )
 }
 
 const styles = StyleSheet.create({
     background: {
-        flex: 1,
+
+        width: '100%',
+        height: '100%',
         alignItems: 'center',
         justifyContent: 'center'
-
     },
     logo: {
-        marginTop: 8,
-        width: 350,
-        height: 350,
-        alignItems: "center",
+        marginTop: 'center',
+
     },
     viewRegister: {
-        marginTop: 5,
-        alignItems: "center",
-        color: '#ffffff'
+        width: "80%",
+
 
 
     },
     textInput: {
+        marginTop: 20,
         borderBottomColor: '#2f3236',
         borderBottomWidth: 1,
+        textAlign: 'center'
+
+    },
+    btn: {
+        backgroundColor: '#2f3236',
+        padding: 15,
+        borderRadius: 10,
+        marginTop: 40,
+        textAlign: 'center',
+
+    },
+    textBtn: {
+        color: '#ffffff',
+        textAlign: 'center'
+    },
+    picker: {
+        marginTop: 40,
+        borderColor:'#570985',
+        borderWidth:2,
+        backgroundColor:'#ffffff',
+        borderRadius:10
+
     }
+
+
+
 })
 
 //Exportando nosso componente de Cadastro 
