5ef109944f39a29ce16ae4aabeff5fc3b2e11466
Vettore0 <54145455+Vettore0@users.noreply.github.com>
2019-11-18 11:59:57 -0300

    flat list

--- src/routes.js
+++ src/routes.js
@@ -6,19 +6,20 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Perfil from './pages/Perfil'
 import AddFriends from './pages/AddFriends'
+import Conversations from './pages/Conversations'
 
 const routes = createAppContainer(
 
     createSwitchNavigator({
         
-     
+        Conversations,
         Perfil,
         Login,
         Register,
         AddFriends
         
     },{
-        initialRouteName:'AddFriends'
+        initialRouteName:'Conversations'
     })
 )
 
